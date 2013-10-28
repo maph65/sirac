ba6eb6169928287c6e3fdc4061b98bab0974cccd
ENGINETEC <contacto@enginetec.com.mx>
2013-10-27 19:41:36 -0600

    Armando plan de trabajo

---
+++ protected/class/diaSemanaCiclo.php
@@ -0,0 +1,21 @@
+<?php
+class diaSemanaCiclo{
+    /**
+     * ObtenDiaSemanaCiclo
+     * 
+     * Permite obtener el número de día de la semana (1 para lunes, 7 para domingo)
+     * y el número de semana a partir de la fecha de inicio del ciclo al día actual
+     * 
+     * @param String $fechaInicioCiclo String con la fecha de inicio del ciclo con el formato Año-mes-dia [date("Y-m-d")]
+     * @param Array $diaSemana Array con los valores NumDia (Número de día) y NumSemana (Número de semana)
+     */
+    public static function obtenDiaSemanaCiclo($fechaInicioCiclo){
+        $hoy = strtotime(date("Y-m-d"));
+        $inicioCiclo = strtotime($fechaInicioCiclo);
+        $numeroSemana = floor(abs(($hoy-$inicioCiclo)/(60*60*24*7)));
+        $numeroDia = date("N");
+        $diaSemana = array('NumDia'=>$numeroDia, 'NumSemana'=>$numeroSemana);
+        return $diaSemana;
+    }
+}
+?>
--- protected/controller/planTrabajoController.php
+++ protected/controller/planTrabajoController.php
@@ -12,12 +12,20 @@ class planTrabajoController extends DooController {
             $result = array('acceso' => 'correcto');
             $planTrabajo = new htPlanTrabajo();
             $planTrabajo->fecha_visita(date("Y-m-d"));
-            $arrayPlanTrabajo = $this->db()->find($planTrabajo);
+            $arrayPlanTrabajo = $this->db()->relateMany($planTrabajo,array('ctMedico','ctSitio','ctCiclo'));//->find($planTrabajo);
             $cantidadVisitar = sizeof($arrayPlanTrabajo);
             if($cantidadVisitar>0){
                 $result['numeroVisitas'] = $cantidadVisitar;
                 foreach ($arrayPlanTrabajo as $visita){
-                    
+                    print_r($visita);
+                }
+            }else{
+                //No hay datos en el plan de trabajo para hoy, se trata de obtener los datos del ctPlanTrabajo
+                if($this->construirPlanTrabajoDia()){
+                    obtenerPlanTrabajo();
+                    exit;
+                }else{
+                    $result['numeroVisitas'] = 0;
                 }
             }
         } else {
@@ -25,6 +33,33 @@ class planTrabajoController extends DooController {
         }
         //Imprimimos la respuesta como objeto json
         echo json_encode($result);
+    }
+    
+    private function construirPlanTrabajoDia(){
+        Doo::loadModel('htPlanTrabajo');
+        Doo::loadModel('ctPlanTrabajo');
+        Doo::loadModel('ctCiclo');
+        Doo::loadClass('diaSemanaCiclo');
+        $ciclo = $this->db()->find(new ctCiclo(),array('where'=>'fecha_inicio<="'.date("Y-m-d").'" AND fecha_termino>="'.date("Y-m-d").'"', 'limit'=>1));
+        if($ciclo){
+            $diaSemana = diaSemanaCiclo::obtenDiaSemanaCiclo($ciclo->fecha_inicio);
+            $planTrabajo = new ctPlanTrabajo();
+            //Hay que descomentar esto, pues se esta fijando a un día específico
+            $planTrabajo->semana = 3; //$diaSemana['NumSemana'];
+            $planTrabajo->id_dia = 1; //$diaSemana['NumDia'];
+            $arrayPlanTrabajo = $this->db()->find($planTrabajo);
+            if(!empty($arrayPlanTrabajo)){
+                foreach ($arrayPlanTrabajo as $medicoVisita){
+                    
+                }
+            }
+        }else{
+            $result = array('acceso' => 'correcto','error'=>'Ciclo no encontrado para la fecha actual.');
+            echo json_encode($result);
+            return false;
+            exit;
+        }
+        return false;
     }
 
 }
