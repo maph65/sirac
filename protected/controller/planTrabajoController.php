cd4c78d801b21b2c37c3daec16572f84b4cb8764
ENGINETEC <contacto@enginetec.com.mx>
2013-10-27 20:00:36 -0600

    Plan trabajo
    Se crea plan de trabajo

--- protected/controller/planTrabajoController.php
+++ protected/controller/planTrabajoController.php
@@ -21,7 +21,7 @@ class planTrabajoController extends DooController {
                 }
             }else{
                 //No hay datos en el plan de trabajo para hoy, se trata de obtener los datos del ctPlanTrabajo
-                if($this->construirPlanTrabajoDia()){
+                if($this->construirPlanTrabajoDia($usuario)){
                     obtenerPlanTrabajo();
                     exit;
                 }else{
@@ -35,11 +35,12 @@ class planTrabajoController extends DooController {
         echo json_encode($result);
     }
     
-    private function construirPlanTrabajoDia(){
+    private function construirPlanTrabajoDia($usuario){
         Doo::loadModel('htPlanTrabajo');
         Doo::loadModel('ctPlanTrabajo');
         Doo::loadModel('ctCiclo');
         Doo::loadClass('diaSemanaCiclo');
+        //$ciclo = new ctCiclo();
         $ciclo = $this->db()->find(new ctCiclo(),array('where'=>'fecha_inicio<="'.date("Y-m-d").'" AND fecha_termino>="'.date("Y-m-d").'"', 'limit'=>1));
         if($ciclo){
             $diaSemana = diaSemanaCiclo::obtenDiaSemanaCiclo($ciclo->fecha_inicio);
@@ -47,11 +48,27 @@ class planTrabajoController extends DooController {
             //Hay que descomentar esto, pues se esta fijando a un día específico
             $planTrabajo->semana = 3; //$diaSemana['NumSemana'];
             $planTrabajo->id_dia = 1; //$diaSemana['NumDia'];
+            $planTrabajo->id_usuario = $usuario;
             $arrayPlanTrabajo = $this->db()->find($planTrabajo);
             if(!empty($arrayPlanTrabajo)){
                 foreach ($arrayPlanTrabajo as $medicoVisita){
-                    
+                    $htPlan = new htPlanTrabajo();
+                    $htPlan->id_usuario = $usuario;
+                    $htPlan->fecha_visita = date("Y-m-d");
+                    $htPlan->id_ciclo = $ciclo->id_ciclo;
+                    $htPlan->id_medico = $medicoVisita->id_medico;
+                    $htPlan->id_sitio = $medicoVisita->id_sitio;
+                    $htPlan->activo = 0; //No esta activo el plan del día, debe ser activado
+                    $htPlan->hora_visita = "00:00:00"; //No se define la hora de visita, debe definirla el usuario
+                    //Procedemos a almancenar en la tabla de htPlanTrabajo
+                    $htPlan->insert();
                 }
+                //Se ha actualizado exitosamente, podemos volver al flujo inicial
+                return true;
+                exit;
+            }else{
+                //No ha registros que insertar, no insertamos nada
+                return false;
             }
         }else{
             $result = array('acceso' => 'correcto','error'=>'Ciclo no encontrado para la fecha actual.');
